cdd1388735b785ed0ccae4dab7e96c6d239ccee6
IgnacioBritoss <167232030+IgnacioBritoss@users.noreply.github.com>
2024-10-09 12:08:06 -0300

    listoooooo

--- frontend/estilo.js
+++ frontend/estilo.js
@@ -1,9 +1,24 @@
 
 const bti = document.getElementById("pasar");
+const btr = document.getElementById("registerButton")
 const contenedor = document.getElementById("contenidoc");
+const bts = document.getElementById("loginButton")
 
 bti.addEventListener("click", function() {
-    contenedor.style.display = "block";
+    formregistro.style.display = "block";
     bti.style.display = "none"
     document.body.style.background = "white";
 });
+
+btr.addEventListener("click", function() {
+    formregistro.style.display = "none";
+    formsecion.style.display = "block";
+    document.body.style.background = "white";
+});
+
+bts.addEventListener("click", function() {
+    contenedor.style.display = "block";
+    formsecion.style.display = "none";
+    document.body.style.background = "white";
+});
+
